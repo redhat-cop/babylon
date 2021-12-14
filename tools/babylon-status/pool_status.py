a3a4bcb0476b9fc2899dbcec933c86bd936b4e61
Guillaume Coré <gucore@redhat.com>
2021-12-14 09:19:41 +0100

    pool status script: add pattern, total, and taken

--- tools/babylon-status/pool_status.py
+++ tools/babylon-status/pool_status.py
@@ -14,6 +14,9 @@ from datetime import datetime
 from pprint import pprint
 import urllib3
 
+
+pool_pattern = sys.argv[1]
+
 urllib3.disable_warnings()
 
 kubernetes.config.load_kube_config()
@@ -28,9 +31,14 @@ response_pools = custom_objects_api.list_namespaced_custom_object(
     'resourcepools')
 
 pools = response_pools['items']
+ttotal = 0
+tavailable = 0
+ttaken = 0
 
-print("POOL MIN TOTAL AVAILABLE")
+print("POOL MIN TOTAL AVAILABLE TAKEN")
 for pool in pools:
+    if pool_pattern and pool_pattern not in pool['metadata']['name']:
+        continue
     #status_resources = pool.get('status', {}).get('resources', [])
     label = 'poolboy.gpte.redhat.com/resource-pool-name='+pool['metadata']['name']
     handles_resp = custom_objects_api.list_namespaced_custom_object(
@@ -42,27 +50,44 @@ for pool in pools:
     handles = handles_resp['items']
     min_available = pool['spec']['minAvailable']
     total = 0
-    completed = 0
-    failed = 0
-    inprogress = 0
-    unknown = 0
+    available = 0
+    taken = 0
 
     for handle in handles:
+        total = total + 1
+        ttotal = ttotal + 1
+
+        if 'resourceClaim' in handle['spec']:
+            taken = taken + 1
+            ttaken = ttaken + 1
+            continue
+
+        if 'resources' not in handle['spec']:
+            continue
+
+        totalresource = len(handle['spec']['resources'])
+        resourcecompleted = 0
+
         for resource in handle['spec']['resources']:
             try:
                 if resource['reference']['kind'] == 'AnarchySubject':
-                    total = total + 1
                     subject = custom_objects_api.get_namespaced_custom_object(
                       'anarchy.gpte.redhat.com', 'v1', resource['reference']['namespace'], 'anarchysubjects', resource['reference']['name'])
                     try:
                         if subject['spec']['vars']['desired_state'] == subject['spec']['vars']['current_state']:
                             if subject['spec']['vars']['healthy'] == True:
-                              completed = completed + 1
+                              resourcecompleted = resourcecompleted + 1
                     except:
                         pass
             except:
                 pass
 
+        if resourcecompleted == len(handle['spec']['resources']):
+            available = available + 1
+            tavailable = tavailable + 1
 
 
-    print("%s %s %s %s" %(pool['metadata']['name'], min_available, total, completed))
+
+    print("%s %s %s %s %s" %(pool['metadata']['name'], min_available, total, available, taken))
+
+print("%s %s %s %s %s" %('TOTAL', '-', ttotal, tavailable, ttaken))
