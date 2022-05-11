028ad34f5f54c71d06b6d7f762a8d2f7a984bc02
Aleix Casanovas <acasanov@redhat.com>
2022-05-11 19:06:29 +0200

    fixup! remove enzyme dep

--- catalog/ui/test-setup.js
+++ catalog/ui/test-setup.js
@@ -1,5 +1,1 @@
-import { configure } from 'enzyme';
-// import ReactSixteenAdapter from 'enzyme-adapter-react-16';
-import ReactSeventeenAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-configure({ adapter: new ReactSeventeenAdapter() });
+import '@testing-library/jest-dom';
