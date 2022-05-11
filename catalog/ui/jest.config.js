38829f35cdecc46fe3dc56f38ad4ad79f3365d32
Aleix <acasanov@redhat.com>
2022-05-11 21:09:39 +0200

    Remove enzyme library & upgrade tests using react-testing-library (#298)
    * remove enzyme dep
    
    * fixup! remove enzyme dep
    
    * fixup! fixup! remove enzyme dep

--- catalog/ui/jest.config.js
+++ catalog/ui/jest.config.js
@@ -40,7 +40,4 @@ module.exports = {
 
   // The test environment that will be used for testing.
   testEnvironment: 'jsdom',
-
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
--- catalog/ui/src/app/app.spec.tsx
+++ catalog/ui/src/app/app.spec.tsx
@@ -1,51 +1,37 @@
 import * as React from 'react';
 import App from '@app/index';
-import { mount, shallow } from 'enzyme';
-import { Button } from '@patternfly/react-core';
-import { Provider } from 'react-redux';
-import { store } from '@app/store';
+import { render, fireEvent } from './utils/test-utils';
 
-function AppWithReduxStore() {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-}
 describe('App tests', () => {
-  test('should render default App component', () => {
-    const view = shallow(<App />);
-    expect(view).toMatchSnapshot();
-  });
-
   it('should render a nav-toggle button', () => {
-    const wrapper = mount(<AppWithReduxStore />);
-    const button = wrapper.find(Button);
-    expect(button.exists()).toBe(true);
+    const { container } = render(<App />);
+    const button = container.querySelector('#nav-toggle');
+    expect(button).toBeInTheDocument();
   });
 
   it('should hide the sidebar on smaller viewports', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 600 });
-    const wrapper = mount(<AppWithReduxStore />);
-    window.dispatchEvent(new Event('resize'));
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
+    const { container } = render(<App />);
+    fireEvent(window, new Event('resize'));
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-collapsed')).toBeTruthy();
   });
 
   it('should expand the sidebar on larger viewports', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 1200 });
-    const wrapper = mount(<AppWithReduxStore />);
-    window.dispatchEvent(new Event('resize'));
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
+    const { container } = render(<App />);
+    console.log(window.innerWidth);
+    fireEvent.resize(window);
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-expanded')).toBeTruthy();
   });
 
-  it('should hide the sidebar when clicking the nav-toggle button', () => {
+  it('should hide the sidebar when clicking the nav-toggle button', async () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 1200 });
-    const wrapper = mount(<AppWithReduxStore />);
-    window.dispatchEvent(new Event('resize'));
-    const button = wrapper.find('#nav-toggle').hostNodes();
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
-    button.simulate('click');
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeFalsy();
+    const { container } = render(<App />);
+    fireEvent(window, new Event('resize'));
+    const button = container.querySelector('#nav-toggle');
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-expanded')).toBeTruthy();
+    fireEvent.click(button);
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-collapsed')).toBeTruthy();
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-expanded')).toBeFalsy();
   });
 });
