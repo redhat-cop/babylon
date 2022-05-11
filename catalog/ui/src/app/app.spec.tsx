06cd63f213dfc935f35ca7dfefa5bca80810960a
Aleix Casanovas <acasanov@redhat.com>
2022-05-11 19:01:44 +0200

    remove enzyme dep

--- catalog/ui/src/app/app.spec.tsx
+++ catalog/ui/src/app/app.spec.tsx
@@ -4,6 +4,7 @@ import { mount, shallow } from 'enzyme';
 import { Button } from '@patternfly/react-core';
 import { Provider } from 'react-redux';
 import { store } from '@app/store';
+import { render, fireEvent } from './utils/test-utils';
 
 function AppWithReduxStore() {
   return (
@@ -26,16 +27,16 @@ describe('App tests', () => {
 
   it('should hide the sidebar on smaller viewports', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 600 });
-    const wrapper = mount(<AppWithReduxStore />);
-    window.dispatchEvent(new Event('resize'));
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
+    const { container } = render(<App />);
+    fireEvent.resize(window);
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-collapsed')).toBeTruthy();
   });
 
   it('should expand the sidebar on larger viewports', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 1200 });
-    const wrapper = mount(<AppWithReduxStore />);
-    window.dispatchEvent(new Event('resize'));
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
+    const { container } = render(<AppWithReduxStore />);
+    fireEvent.resize(window);
+    expect(container.querySelector('#page-sidebar').classList.contains('pf-m-expanded')).toBeTruthy();
   });
 
   it('should hide the sidebar when clicking the nav-toggle button', () => {
