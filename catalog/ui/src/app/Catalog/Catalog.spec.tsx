e6dace1097da590276456997c7d3ab812aa6c97a
Aleix Casanovas <acasanov@redhat.com>
2022-05-19 13:09:50 +0200

    fixup! Unify onMount/onRefresh useEffects

--- catalog/ui/src/app/Catalog/Catalog.spec.tsx
+++ catalog/ui/src/app/Catalog/Catalog.spec.tsx
@@ -1,12 +1,12 @@
 import '@testing-library/jest-dom';
 import * as React from 'react';
-import { render, waitFor, fireEvent } from '../utils/test-utils';
+import { render, waitFor, fireEvent, act } from '../utils/test-utils';
 import { within } from '@testing-library/dom';
 import Catalog from './Catalog';
 import catalogItemsObj from '../__mocks__/catalogItems.json';
 import { CatalogItemList, CatalogNamespace } from '@app/types';
+import { createMemoryHistory } from 'history';
 
-const mockHistoryPush = jest.fn();
 jest.mock('@app/api', () => {
   return {
     listCatalogItems: jest.fn(() => Promise.resolve(catalogItemsObj as CatalogItemList)),
@@ -16,9 +16,6 @@ jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
   useParams: () => ({ namespace: 'fakeNamespace' }),
   useRouteMatch: () => ({ url: '/catalog/fakeNamespace', params: { namespace: 'fakeNamespace' } }),
-  useHistory: () => ({
-    push: mockHistoryPush,
-  }),
 }));
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
@@ -41,16 +38,25 @@ describe('Catalog Component', () => {
   });
   test('When filtering should save the selection in sessionStorage', async () => {
     const setItemSpy = jest.spyOn(Object.getPrototypeOf(window.sessionStorage), 'setItem');
-    const { container } = render(<Catalog />);
+    const history = createMemoryHistory();
+    const { container } = render(<Catalog />, { history: history });
     const withinCategorySelector = within(container.querySelector('.catalog-category-selector'));
     await waitFor(() => expect(withinCategorySelector.getByText('Other')).toBeInTheDocument());
     fireEvent.click(withinCategorySelector.getByText('Other').closest('button'));
     expect(setItemSpy).toBeCalledWith('lastCatalogFilter', 'category=Other');
-    expect(mockHistoryPush).toHaveBeenCalledWith('/?category=Other');
+    await waitFor(() =>
+      expect(withinCategorySelector.getByText('Other').closest('button').getAttribute('aria-selected')).toBe('true')
+    );
+    expect(history.location.search).toBe('?category=Other');
   });
   test('When rendering the /catalog with a saved filter should redirect', async () => {
     window.sessionStorage.setItem('lastCatalogFilter', 'category=Other');
-    render(<Catalog />);
-    expect(mockHistoryPush).toHaveBeenCalledWith('/?category=Other');
+    const history = createMemoryHistory();
+    const { container } = render(<Catalog />, { history: history });
+    const withinCategorySelector = within(container.querySelector('.catalog-category-selector'));
+    await waitFor(() =>
+      expect(withinCategorySelector.getByText('Other').closest('button').getAttribute('aria-selected')).toBe('true')
+    );
+    expect(history.location.search).toBe('?category=Other');
   });
 });
