b518cc4e45d78bf7294cc7de3168f528a3138311
Aleix <acasanov@redhat.com>
2022-07-05 15:22:19 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,7 +4,7 @@ app_name=$1
 image=$2
 namespace=$3
 
-deploy=`oc get deployment $app_name`
+deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
     oc set image deployment/$app_name $app_name=$image
     oc rollout restart deployment/$app_name
