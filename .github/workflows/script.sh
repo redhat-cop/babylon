5922a29dde3b06336265676f0e69509c5f0e31f9
Aleix Casanovas <acasanov@redhat.com>
2022-07-05 16:55:12 +0200

    Fix script

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -3,10 +3,11 @@
 app_name=$1
 image=$2
 namespace=$3
+container_name=$4
 
 deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
-    oc set image deployment/$app_name $app_name=$image -n $namespace
+    oc set image deployment/$app_name $container_name=$image -n $namespace
     oc rollout restart deployment/$app_name -n $namespace
 else
     oc new-app $image --name $app_name -n $namespace
