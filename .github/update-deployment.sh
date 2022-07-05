61d3eae13de26b3f1a37862a538aa9902e3223d6
Aleix Casanovas <acasanov@redhat.com>
2022-07-05 18:07:33 +0200

    Apply comments

--- .github/update-deployment.sh
+++ .github/update-deployment.sh
@@ -9,6 +9,4 @@ deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
     oc set image deployment/$app_name $container_name=$image -n $namespace
     oc rollout restart deployment/$app_name -n $namespace
-else
-    oc new-app $image --name $app_name -n $namespace
 fi
