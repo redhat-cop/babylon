6cbd93c5d3d4dbedb6abb6b5b237afb44398e6b8
Aleix Casanovas <acasanov@redhat.com>
2022-07-11 17:01:54 +0200

    fixup! Get amount spent of a service

--- catalog/ui/src/app/Services/ServiceActions.tsx
+++ catalog/ui/src/app/Services/ServiceActions.tsx
@@ -71,11 +71,7 @@ const ServiceActions: React.FC<{
 
   if (actionHandlers.getCost) {
     actionDropdownItems.push(
-      <ActionDropdownItem
-        key="getCost"
-        label={serviceName ? `Get amount spent ${serviceName}` : 'Get amount spent'}
-        onSelect={() => actionHandlers.getCost()}
-      />
+      <ActionDropdownItem key="getCost" label="Get amount spent" onSelect={() => actionHandlers.getCost()} />
     );
   }
   return (
