b583124d9bfac4e775b3d8bbfbc8aeb498499dc9
Aleix <acasanov@redhat.com>
2022-09-01 17:51:02 +0200

    Add Footer to workshop user interface (#551)

--- catalog/ui/src/app/Workshop/Workshop.tsx
+++ catalog/ui/src/app/Workshop/Workshop.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { Page } from '@patternfly/react-core';
 import { workshopLogin, WorkshopDetails } from './workshopApi';
 import WorkshopAccess from './WorkshopAccess';
@@ -7,11 +7,15 @@ import WorkshopHeader from './WorkshopHeader';
 import WorkshopLogin from './WorkshopLogin';
 import useSWRImmutable from 'swr/immutable';
 import { apiPaths, fetcher } from './workshop-utils';
+import Footer from '@app/components/Footer';
+import summitLogo from '@app/bgimages/Summit-Logo.svg';
 
 import './workshop.css';
 
 const Workshop: React.FC = () => {
   const { workshopId } = useParams();
+  const { search } = useLocation();
+  const userInterface = new URLSearchParams(search).get('userInterface');
   const [loginFailureMessage, setLoginFailureMessage] = useState('');
   const { data: workshop } = useSWRImmutable<WorkshopDetails>(
     workshopId ? apiPaths.WORKSHOP({ workshopId }) : null,
@@ -37,23 +41,28 @@ const Workshop: React.FC = () => {
     }
   }
 
-  if (workshopPrivateInfo.assignment) {
-    return (
-      <Page header={<WorkshopHeader />} className="workshop__page">
+  return (
+    <Page header={<WorkshopHeader userInterface={userInterface} />} style={{ backgroundColor: '#fff' }}>
+      {workshopPrivateInfo.assignment ? (
         <WorkshopAccess workshop={workshopPrivateInfo} />
-      </Page>
-    );
-  } else {
-    return (
-      <Page header={<WorkshopHeader />} className="workshop__page">
+      ) : (
         <WorkshopLogin
           loginFailureMessage={loginFailureMessage}
           onLogin={(email, accessPassword) => attemptLogin(email, accessPassword)}
           workshop={workshop}
         />
-      </Page>
-    );
-  }
+      )}
+      <Footer
+        rightElement={
+          userInterface === 'summit' ? (
+            <a href="https://www.redhat.com/summit">
+              <img src={summitLogo} alt="Red Hat Summit" width="72px" />
+            </a>
+          ) : null
+        }
+      />
+    </Page>
+  );
 };
 
 export default Workshop;
