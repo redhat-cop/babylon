8ef6a64aabc4dba87c6be72e15d70f884f1bef08
Aleix <acasanov@redhat.com>
2022-09-01 12:08:49 +0200

    Summit header for Workshops (#549)
    * Workshop summit header

--- catalog/ui/src/app/Workshop/WorkshopHeader.tsx
+++ catalog/ui/src/app/Workshop/WorkshopHeader.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { PageHeader } from '@patternfly/react-core';
 import UserInterfaceLogo from '@app/components/UserInterfaceLogo';
 import summitLogo from '@app/bgimages/Summit-Logo.svg';
+import redHatLogo from '@app/bgimages/RedHat-Logo.svg';
 
 const WorkshopHeader: React.FC = () => {
-  const navigate = useNavigate();
   const { search } = useLocation();
   const userInterface = new URLSearchParams(search).get('userInterface');
   function LogoImg() {
     if (userInterface === 'summit') {
       return (
-        <img
-          src={summitLogo}
-          onClick={() => navigate('/')}
-          alt="Red Hat Summit"
-          className="summit-logo"
-          style={{ height: '48px' }}
-        />
+        <a href="https://summit.demo.redhat.com/" style={{ display: 'flex', width: '70px' }}>
+          <img src={summitLogo} alt="Red Hat Summit" />
+        </a>
       );
     }
-    return <UserInterfaceLogo onClick={() => navigate('/')} style={{ width: '278px' }} />;
+    return (
+      <a href="/" style={{ width: '278px', display: 'flex' }}>
+        <UserInterfaceLogo />
+      </a>
+    );
   }
-  return <PageHeader className="workshop" logo={<LogoImg />} />;
+  return (
+    <div style={{ backgroundColor: 'var(--pf-c-page__header--BackgroundColor)', gridArea: 'header' }}>
+      <PageHeader
+        className="workshop"
+        logo={<LogoImg />}
+        style={{ maxWidth: '1170px', margin: '0 auto', width: '100%' }}
+        headerTools={
+          userInterface === 'summit' ? (
+            <a href="https://redhat.com/" style={{ width: '138px', marginLeft: 'auto', display: 'flex' }}>
+              <img src={redHatLogo} alt="Red Hat" />
+            </a>
+          ) : null
+        }
+      ></PageHeader>
+    </div>
+  );
 };
 
 export default WorkshopHeader;
