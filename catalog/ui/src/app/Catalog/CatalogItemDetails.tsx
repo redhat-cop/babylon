c5e7d2ea7547f9d2debb9c0c1d4b5ba0e29d4381
Aleix Casanovas <acasanov@redhat.com>
2023-01-19 14:08:07 +0100

    Include auto-stop / auto-destroy times

--- catalog/ui/src/app/Catalog/CatalogItemDetails.tsx
+++ catalog/ui/src/app/Catalog/CatalogItemDetails.tsx
@@ -56,6 +56,7 @@ import CatalogItemIcon from './CatalogItemIcon';
 import CatalogItemHealthDisplay from './CatalogItemHealthDisplay';
 
 import './catalog-item-details.css';
+import parseDuration from 'parse-duration';
 
 enum CatalogItemAccess {
   Allow,
@@ -70,7 +71,6 @@ const CatalogItemDetails: React.FC<{ catalogItem: CatalogItem; onClose: () => vo
   const { provisionTimeEstimate, accessControl, lastUpdate } = catalogItem.spec;
   const { labels, namespace, name } = catalogItem.metadata;
   const provider = getProvider(catalogItem);
-  const sla = getSLA(catalogItem);
   const stage = getStage(catalogItem);
   const catalogItemName = displayName(catalogItem);
   const { description, descriptionFormat } = getDescription(catalogItem);
@@ -108,6 +108,8 @@ const CatalogItemDetails: React.FC<{ catalogItem: CatalogItem; onClose: () => vo
   const incidentUrl = getIncidentUrl(catalogItem);
   const rating = getRating(catalogItem);
   const accessCheckResult = checkAccessControl(accessControl, groups);
+  const autoStopTime = catalogItem.spec.runtime?.default;
+  const autoDestroyTime = catalogItem.spec.lifespan?.default;
   const catalogItemAccess: CatalogItemAccess =
     isAdmin || isLabDeveloper(groups)
       ? CatalogItemAccess.Allow
@@ -283,10 +285,17 @@ const CatalogItemDetails: React.FC<{ catalogItem: CatalogItem; onClose: () => vo
                 </DescriptionListGroup>
               ) : null}
 
-              {sla && stage === 'prod' ? (
-                <DescriptionListGroup className="catalog-item-details__sla">
-                  <DescriptionListTerm>SLA</DescriptionListTerm>
-                  <DescriptionListDescription>{sla.replace(/_+/g, ' | ')}</DescriptionListDescription>
+              {autoStopTime ? (
+                <DescriptionListGroup className="catalog-item-details__auto-stop">
+                  <DescriptionListTerm>Auto-Stop</DescriptionListTerm>
+                  <DescriptionListDescription>{parseDuration(autoStopTime)}</DescriptionListDescription>
+                </DescriptionListGroup>
+              ) : null}
+
+              {autoDestroyTime ? (
+                <DescriptionListGroup className="catalog-item-details__auto-destroy">
+                  <DescriptionListTerm>Auto-Destroy</DescriptionListTerm>
+                  <DescriptionListDescription>{parseDuration(autoDestroyTime)}</DescriptionListDescription>
                 </DescriptionListGroup>
               ) : null}
             </DescriptionList>
