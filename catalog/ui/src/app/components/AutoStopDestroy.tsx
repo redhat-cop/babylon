e95c5bc336988ec1db6e2f381e63ad93dc709b8b
Aleix Casanovas <acasanov@redhat.com>
2023-01-19 17:09:19 +0100

    fixup! Show no idle

--- catalog/ui/src/app/components/AutoStopDestroy.tsx
+++ catalog/ui/src/app/components/AutoStopDestroy.tsx
@@ -27,53 +27,12 @@ const AutoStopDestroy: React.FC<{
   isDisabled: _isDisabled,
   notDefinedMessage,
 }) => {
-  const workshopProvisionName = resourceClaim?.metadata?.labels?.[`${BABYLON_DOMAIN}/workshop-provision`];
-  const isPartOfWorkshop = !!workshopProvisionName;
   let time = _time;
-  if (typeof time !== 'number') {
-    time = new Date(_time).getTime();
-  }
   let isDisabled = _isDisabled;
-  if (isDisabled === null || typeof isDisabled === 'undefined') {
-    if (type === 'auto-stop') {
-      isDisabled = !checkResourceClaimCanStop(resourceClaim) || isPartOfWorkshop;
-    }
-    if (type === 'auto-destroy') {
-      isDisabled = !resourceClaim.status?.lifespan || isPartOfWorkshop;
-    }
-  }
-  console.log(time);
 
-  return (
-    <span>
-      {time ? (
-        <>
-          <Button
-            variant="control"
-            isDisabled={isDisabled}
-            onClick={onClick}
-            icon={<OutlinedClockIcon />}
-            iconPosition="right"
-            className={className}
-            isSmall
-          >
-            {type === 'auto-stop' && time > Date.now() + 15778800000 ? ( // If is more than 6months show no idle
-              <span style={{ marginRight: 'var(--pf-global--spacer--sm)' }}>No idle</span>
-            ) : (
-              <>
-                <LocalTimestamp variant="short" time={time} />
-                {variant === 'extended' ? (
-                  <span style={{ padding: '0 6px' }}>
-                    (
-                    <TimeInterval toEpochMilliseconds={time} />)
-                  </span>
-                ) : null}
-              </>
-            )}
-          </Button>
-          {children}
-        </>
-      ) : notDefinedMessage ? (
+  if (!time) {
+    return notDefinedMessage ? (
+      <span>
         <Button
           variant="control"
           isDisabled={isDisabled}
@@ -85,9 +44,54 @@ const AutoStopDestroy: React.FC<{
         >
           <span style={{ marginRight: 'var(--pf-global--spacer--sm)' }}>{notDefinedMessage}</span>
         </Button>
-      ) : (
-        <span>-</span>
-      )}
+        {children}
+      </span>
+    ) : (
+      <span>-</span>
+    );
+  }
+
+  const workshopProvisionName = resourceClaim?.metadata?.labels?.[`${BABYLON_DOMAIN}/workshop-provision`];
+  const isPartOfWorkshop = !!workshopProvisionName;
+  if (typeof time !== 'number') {
+    time = new Date(_time).getTime();
+  }
+
+  if (isDisabled === null || typeof isDisabled === 'undefined') {
+    if (type === 'auto-stop') {
+      isDisabled = !checkResourceClaimCanStop(resourceClaim) || isPartOfWorkshop;
+    }
+    if (type === 'auto-destroy') {
+      isDisabled = !resourceClaim.status?.lifespan || isPartOfWorkshop;
+    }
+  }
+
+  return (
+    <span>
+      <Button
+        variant="control"
+        isDisabled={isDisabled}
+        onClick={onClick}
+        icon={<OutlinedClockIcon />}
+        iconPosition="right"
+        className={className}
+        isSmall
+      >
+        {type === 'auto-stop' && time > Date.now() + 15778800000 ? ( // If is more than 6months show no idle
+          <span style={{ marginRight: 'var(--pf-global--spacer--sm)' }}>No idle</span>
+        ) : (
+          <>
+            <LocalTimestamp variant="short" time={time} />
+            {variant === 'extended' ? (
+              <span style={{ padding: '0 6px' }}>
+                (
+                <TimeInterval toEpochMilliseconds={time} />)
+              </span>
+            ) : null}
+          </>
+        )}
+      </Button>
+      {children}
     </span>
   );
 };
