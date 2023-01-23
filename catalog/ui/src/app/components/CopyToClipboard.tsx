6b068dc313e1ff169960021b225da96704b91829
Aleix Casanovas <acasanov@redhat.com>
2023-01-23 17:13:30 +0100

    fixup! Share catalog item button

---
+++ catalog/ui/src/app/components/CopyToClipboard.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import CopyIcon from '@patternfly/react-icons/dist/js/icons/copy-icon';
+import { Button, TextInput, Tooltip } from '@patternfly/react-core';
+
+const CopyToClipboard: React.FC<{
+  text: string;
+}> = ({ text }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = useCallback(async () => {
+    await navigator.clipboard.writeText(text);
+    setCopied(true);
+  }, [text]);
+
+  useEffect(() => {
+    let timer = null;
+    if (copied) {
+      timer = setTimeout(setCopied, 2000, false);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [copied]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <TextInput type="text" isDisabled value={text} aria-label={text} />
+      <Tooltip position="bottom" content={<div>{copied ? `Copied` : `Copy to clipboard`}</div>}>
+        <Button variant="control" aria-label="Copy" onClick={handleCopy}>
+          <CopyIcon />
+        </Button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default CopyToClipboard;
