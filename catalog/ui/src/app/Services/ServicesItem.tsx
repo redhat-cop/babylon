2b6648114d49c4f1975706610881693c3244c129
Aleix <acasanov@redhat.com>
2023-02-13 11:35:37 +0100

    Do not render asciidoc for provisionMessages (#914)
    * Do not render asciidoc for provisionMessages
    
    * fixup! Do not render asciidoc for provisionMessages

--- catalog/ui/src/app/Services/ServicesItem.tsx
+++ catalog/ui/src/app/Services/ServicesItem.tsx
@@ -108,21 +108,18 @@ const ComponentDetailsList: React.FC<{
   provisionMessages,
   provisionDataEntries,
 }) => {
+  const _provisionMessages =
+    typeof provisionMessages === 'string' ? provisionMessages : provisionMessages ? provisionMessages.join('\n') : null;
   const provisionMessagesHtml = useMemo(
     () =>
       provisionMessages ? (
         <div
           dangerouslySetInnerHTML={{
-            __html: renderContent(
-              (typeof provisionMessages === 'string' ? provisionMessages : provisionMessages.join('\n'))
-                .replace(/^\s+|\s+$/g, '')
-                .replace(/([^\n])\n(?!\n)/g, '$1 +\n'),
-              { format: 'asciidoc' }
-            ),
+            __html: renderContent(_provisionMessages, { format: 'htmlString' }),
           }}
         />
       ) : null,
-    [JSON.stringify(provisionMessages)]
+    [_provisionMessages]
   );
   return (
     <DescriptionList isHorizontal>
