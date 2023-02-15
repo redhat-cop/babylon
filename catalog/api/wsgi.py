e17444a1b775a374963c645b8c72420dba42062f
Patrick T. Rutledge III <rut31337@users.noreply.github.com>
2023-02-15 09:20:45 -0500

    Update wsgi.py with CDH (#924)
    * Update wsgi.py
    
    * Update wsgi.py

--- catalog/api/wsgi.py
+++ catalog/api/wsgi.py
@@ -465,6 +465,11 @@ def salesforce_validation(salesforce_id):
                                             "  Id, StartDate, EndDate, IsActive "
                                             "FROM Campaign "
                                             "WHERE Id = {}", salesforce_id)
+        elif salesforce_id.isnumeric() and len(salesforce_id) == 6:
+            salesforce_type = 'cdh'
+            opportunity_query = format_soql("SELECT Account.CDHPartyNumber__c, IsActive"
+                                            "FROM Opportunity"
+                                            "WHERE Account.CDHPartyNumber__c = {}", salesforce_id)
         else:
             salesforce_type = 'opportunity'
             opportunity_query = format_soql("SELECT "
@@ -497,7 +502,7 @@ def salesforce_validation(salesforce_id):
     if saleforce_valid == 0:
         return False
     else:
-        if salesforce_type == 'campaign':
+        if salesforce_type == 'campaign' or salesforce_type == 'cdh':
             # Business rules for a invalid Campaign
             # if IsActive is false
             is_active = opportunity_info.get('IsActive', False)
