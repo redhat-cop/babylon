5f84c565199222867c48787fbb1d0640445f673c
Aleix Casanovas <acasanov@redhat.com>
2023-03-09 17:54:52 +0100

    Bugfix: Minor changes

--- catalog/ui/src/app/AppLayout/Navigation.tsx
+++ catalog/ui/src/app/AppLayout/Navigation.tsx
@@ -37,8 +37,8 @@ const Navigation: React.FC = () => {
       <NavLink
         to={`/services/${userNamespace.name}`}
         className={
-          locationStartsWith(`/services/${userNamespace.name}`) ||
-          locationStartsWith(`/workshops/${userNamespace.name}`)
+          location.pathname.match(/\/services\/[a-zA-Z0-9_.-]/) ||
+          location.pathname.match(/\/workshops\/[a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+/)
             ? 'pf-m-current'
             : ''
         }
