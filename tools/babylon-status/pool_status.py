6c1dd84ea57baa7ba9a6973c44cd314a5ac5a3c5
Marcos Amorim <mamorim@redhat.com>
2023-03-27 12:07:31 -0400

    Add option to set resource pool minAvailable (#1034)
    * Add option to set resource pool minAvailable
    
    * Remove global variables
    
    * Remove await
    
    * Move load_kube_config

--- tools/babylon-status/pool_status.py
+++ tools/babylon-status/pool_status.py
@@ -1,96 +1,192 @@
 #!/usr/bin/env python3
-
-import json
 import os
 import kubernetes
-import logging
-import requests
-import subprocess
-import sys
-import time
-
-from base64 import b64decode
-from datetime import datetime
-from pprint import pprint
+import pandas as pd
 import urllib3
-
-
-if len(sys.argv) == 2:
-    pool_pattern = str(sys.argv[1])
-else:
-    pool_pattern = ''
+import argparse
 
 urllib3.disable_warnings()
-
+poolboy_domain = 'poolboy.gpte.redhat.com'
+poolboy_version = 'v1'
+poolboy_namespace = 'poolboy'
 kubernetes.config.load_kube_config()
-core_v1_api = kubernetes.client.CoreV1Api()
-custom_objects_api = kubernetes.client.CustomObjectsApi()
-logger = logging.getLogger(sys.argv[0])
 
-response_pools = custom_objects_api.list_namespaced_custom_object(
-    'poolboy.gpte.redhat.com',
-    'v1',
-    'poolboy',
-    'resourcepools')
 
-pools = response_pools['items']
-ttotal = 0
-tavailable = 0
-ttaken = 0
+def parse_args():
+    parser = argparse.ArgumentParser(description="List and update Resource Pools")
+    parser.add_argument('--list',
+                        help='Lists all pools. [default: %(default)s]',
+                        action="store_true",
+                        default=True,
+                        required=False)
 
-print("POOL MIN TOTAL AVAILABLE TAKEN")
-for pool in pools:
-    if pool_pattern and pool_pattern not in pool['metadata']['name']:
-        continue
-    #status_resources = pool.get('status', {}).get('resources', [])
-    label = 'poolboy.gpte.redhat.com/resource-pool-name='+pool['metadata']['name']
-    handles_resp = custom_objects_api.list_namespaced_custom_object(
-                   'poolboy.gpte.redhat.com',
-                   'v1',
-                   'poolboy',
-                   'resourcehandles',
-                   label_selector=label)
-    handles = handles_resp['items']
-    min_available = pool['spec']['minAvailable']
-    total = 0
-    available = 0
-    taken = 0
+    parser.add_argument('--pattern',
+                        dest="pool_pattern",
+                        help='Search pool by pattern',
+                        const='',
+                        nargs='?',
+                        required=False)
 
-    for handle in handles:
-        total = total + 1
-        ttotal = ttotal + 1
+    parser.add_argument('--pool-name',
+                        dest="pool_name",
+                        help='Pool Name',
+                        type=str,
+                        required=False)
 
-        if 'resourceClaim' in handle['spec']:
-            taken = taken + 1
-            ttaken = ttaken + 1
+    parser.add_argument('--set-min',
+                        dest="set_min",
+                        help='Min replica',
+                        type=int,
+                        required=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def search_pool(pool_name):
+    try:
+        custom_objects_api = kubernetes.client.CustomObjectsApi()
+        pool_config = custom_objects_api.get_namespaced_custom_object(poolboy_domain, poolboy_version,
+                                                                      poolboy_namespace,
+                                                                      'resourcepools', pool_name)
+        return pool_config
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            print(f"Resource Pool Not Found {pool_name}")
+            exit(-1)
+        else:
+            print(f"Error connect to K8s API {e}")
+            exit(-2)
+
+
+def set_min(min_available, pool_name):
+    try:
+        custom_objects_api = kubernetes.client.CustomObjectsApi()
+        resource_pool_data = search_pool(pool_name)
+        if len(resource_pool_data) == 0:
+            return
+
+        previous_value = resource_pool_data['spec']['minAvailable']
+        if previous_value == min_available:
+            print(f"Pool {pool_name} already set minAvailable to {previous_value}. Skipping")
+            return
+
+        custom_objects_api.patch_namespaced_custom_object(
+            group=poolboy_domain,
+            version=poolboy_version,
+            namespace=poolboy_namespace,
+            plural='resourcepools',
+            name=pool_name,
+            body={
+                "spec": {
+                    "minAvailable": min_available
+                }
+            }
+        )
+        print(f"Pool {pool_name} updated previous value {previous_value} new value {min_available}")
+
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            print(f"Error apply min for the pool {pool_name}", e)
+            exit(-1)
+        else:
+            print(f"Error connect to K8s API {e}")
+            exit(-2)
+
+
+def list_pools(pool_pattern):
+    custom_objects_api = kubernetes.client.CustomObjectsApi()
+    response_pools = custom_objects_api.list_namespaced_custom_object('poolboy.gpte.redhat.com',
+                                                                      'v1',
+                                                                      'poolboy',
+                                                                      'resourcepools')
+    pools = response_pools.get('items', [])
+    ttotal = 0
+    tavailable = 0
+    ttaken = 0
+    df_pools = pd.DataFrame(columns=['POOL', 'MIN', 'TOTAL', 'AVAILABLE', 'TAKEN'])
+    for pool in pools:
+        pool_name = pool['metadata']['name']
+        if pool_pattern and pool_pattern not in pool_name:
             continue
+        label = f"poolboy.gpte.redhat.com/resource-pool-name={pool_name}"
+        handles_resp = custom_objects_api.list_namespaced_custom_object('poolboy.gpte.redhat.com',
+                                                                        'v1',
+                                                                        'poolboy',
+                                                                        'resourcehandles',
+                                                                        label_selector=label)
+        handles = handles_resp.get('items', [])
+        min_available = pool['spec']['minAvailable']
+        total = 0
+        available = 0
+        taken = 0
 
-        if 'resources' not in handle['spec']:
-            continue
+        for handle in handles:
+            total = total + 1
+            ttotal = ttotal + 1
 
-        totalresource = len(handle['spec']['resources'])
-        resourcecompleted = 0
+            if 'resourceClaim' in handle['spec']:
+                taken = taken + 1
+                ttaken = ttaken + 1
+                continue
 
-        for resource in handle['spec']['resources']:
-            try:
-                if resource['reference']['kind'] == 'AnarchySubject':
-                    subject = custom_objects_api.get_namespaced_custom_object(
-                      'anarchy.gpte.redhat.com', 'v1', resource['reference']['namespace'], 'anarchysubjects', resource['reference']['name'])
-                    try:
-                        if subject['spec']['vars']['desired_state'] == subject['spec']['vars']['current_state']:
-                            if subject['spec']['vars']['healthy'] == True:
-                              resourcecompleted = resourcecompleted + 1
-                    except:
-                        pass
-            except:
-                pass
+            if 'resources' not in handle['spec']:
+                continue
 
-        if resourcecompleted == len(handle['spec']['resources']):
-            available = available + 1
-            tavailable = tavailable + 1
+            resourcecompleted = 0
+
+            for resource in handle['spec']['resources']:
+                try:
+                    if resource['reference']['kind'] == 'AnarchySubject':
+                        subject = custom_objects_api.get_namespaced_custom_object(
+                            'anarchy.gpte.redhat.com', 'v1', resource['reference']['namespace'], 'anarchysubjects',
+                            resource['reference']['name'])
+                        try:
+                            if subject['spec']['vars']['desired_state'] == subject['spec']['vars']['current_state']:
+                                if subject['spec']['vars']['healthy'] == True:
+                                    resourcecompleted = resourcecompleted + 1
+                        except:
+                            pass
+                except:
+                    pass
+
+            if resourcecompleted == len(handle['spec']['resources']):
+                available = available + 1
+                tavailable = tavailable + 1
+
+        pool_dict = {
+            'POOL': pool_name,
+            'MIN': min_available,
+            'TOTAL': total,
+            'AVAILABLE': available,
+            'TAKEN': taken
+        }
+        df_pool = pd.DataFrame([pool_dict])
+
+        df_pools = pd.concat([df_pool, df_pools], ignore_index=True)
+
+    print(df_pools.sort_values('MIN', ascending=False).to_string(index=False))
+    print()
+    print("Total Items: ", df_pools['TOTAL'].sum())
+    print("Total Items With MIN >0: ", df_pools['MIN'].sum())
+    print("Total Available:", df_pools['AVAILABLE'].sum())
+    print("Total Taken: ", df_pools['TAKEN'].sum())
 
 
+def main():
+    args = parse_args()
 
-    print("%s %s %s %s %s" %(pool['metadata']['name'], min_available, total, available, taken))
+    if args.set_min and args.pool_name is None:
+        print("Option --set-min requires --pool-name")
+        exit(-1)
 
-print("%s %s %s %s %s" %('TOTAL', '-', ttotal, tavailable, ttaken))
+    if args.set_min and args.set_min >= 0 and args.pool_name:
+        set_min(args.set_min, args.pool_name)
+        exit(0)
+
+    list_pools(args.pool_pattern)
+
+
+if __name__ == "__main__":
+    main()
