32dd3433f7e0307bdfa3e9821d55c8891371e03d
Aleix <acasanov@redhat.com>
2023-04-05 10:03:45 -0500

    ref: Hide newly created internal label (#1067)

--- catalog/ui/src/app/Catalog/catalog-utils.ts
+++ catalog/ui/src/app/Catalog/catalog-utils.ts
@@ -189,6 +189,7 @@ export const CUSTOM_LABELS: {
 };
 export const HIDDEN_LABELS = [
   'userCatalogItem',
+  'Base_Component',
   CUSTOM_LABELS.DISABLED.key,
   CUSTOM_LABELS.STAGE.key,
   CUSTOM_LABELS.FEATURED_SCORE.key,
@@ -196,6 +197,7 @@ export const HIDDEN_LABELS = [
 ];
 export const HIDDEN_LABELS_DETAIL_VIEW = [
   'userCatalogItem',
+  'Base_Component',
   CUSTOM_LABELS.DISABLED.key,
   CUSTOM_LABELS.STAGE.key,
   CUSTOM_LABELS.FEATURED_SCORE.key,
