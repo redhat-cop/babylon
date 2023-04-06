ed325e393e1d476d3b47acec830ef0238d534045
Aleix <acasanov@redhat.com>
2023-04-06 09:32:05 -0500

    fix: Handle undefined resources (#1073)

--- catalog/ui/src/app/util.ts
+++ catalog/ui/src/app/util.ts
@@ -37,7 +37,7 @@ export function displayName(item: K8sObject | CatalogNamespace | ServiceNamespac
     const catalogItemName = _item.metadata.labels?.[`${BABYLON_DOMAIN}/catalogItemName`];
     const catalogItemDisplayName = _item.metadata.annotations?.[`${BABYLON_DOMAIN}/catalogItemDisplayName`];
 
-    if (_item.spec.resources[0].provider?.name === 'babylon-service-request-configmap') {
+    if (Array.isArray(_item.spec.resources) && _item.spec.resources.length > 0 && _item.spec.resources[0].provider?.name === 'babylon-service-request-configmap') {
       if (catalogItemName && catalogItemDisplayName && _item.metadata.name === catalogItemName) {
         return `${catalogItemDisplayName} Service Request`;
       } else if (catalogItemName && catalogItemDisplayName && _item.metadata.name.startsWith(catalogItemName)) {
