4383cd4c1ba4df1e34eb33bbfca2fd2e7185d461
Aleix <acasanov@redhat.com>
2023-04-19 17:42:49 +0200

    fix: Ignore Z in timestamp (#1118)

--- catalog-manager/operator/operator.py
+++ catalog-manager/operator/operator.py
@@ -42,7 +42,7 @@ async def manage_catalog_item_provision_data(catalog_item, logger):
     )
     catalog_item_last_successful_provision = datetime.fromisoformat(
         catalog_item.annotations.get(
-            Babylon.catalog_item_last_successful_provision, "1970-01-01T00:00:00"
+            Babylon.catalog_item_last_successful_provision[-1], "1970-01-01T00:00:00"
         )
     )
     if (
