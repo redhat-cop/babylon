e9f0e38a891cff631cbec18afc5a66817724230b
Aleix Casanovas <acasanov@redhat.com>
2023-05-31 17:04:14 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat: New activity / purpose fields

--- catalog/ui/src/app/components/ActivityPurposeSelector.tsx
+++ catalog/ui/src/app/components/ActivityPurposeSelector.tsx
@@ -7,40 +7,186 @@ export const ActivityOpts = [
   { name: 'Customer Facing', id: 1 },
   { name: 'Partner Facing', id: 2 },
   { name: 'Practice / Enablement', id: 3 },
-  { name: 'Brand Event', id: 4 },
+  { name: 'Brand Event', id: 4, requiredRoles: [] },
   { name: 'Asset Development', id: 5, requiredRoles: ['rhpds-devs', 'rhpds-admins'] },
   { name: 'Admin', id: 6, requiredRoles: ['rhpds-admins'] },
 ];
 export const PurposeOpts = [
-  { name: 'Conduct a customer Demo', activityId: 1, id: 1, sfdcRequired: true },
-  { name: 'Conduct an individual customer hands on workshop', activityId: 1, id: 2, sfdcRequired: true },
-  { name: 'Conduct a Proof of Concept', activityId: 1, id: 3, sfdcRequired: true },
-  { name: 'Conduct a Marketing Event/Multi customer event', activityId: 1, id: 4, sfdcRequired: true },
-  { name: 'Other', activityId: 1, id: 5, requireUserInput: true, sfdcRequired: true },
-  { name: 'Conduct a Partner Demo', activityId: 2, id: 6, sfdcRequired: true },
-  { name: 'Conduct a Partner Enablement workshop', activityId: 2, id: 7, sfdcRequired: false },
-  { name: 'Conduct a Partner Proof of Concept', activityId: 2, id: 8, sfdcRequired: false },
-  { name: 'Conduct a Multi Partner event', activityId: 2, id: 9, sfdcRequired: false },
-  { name: 'Conduct a Partner event with customers', activityId: 2, id: 10, sfdcRequired: true },
-  { name: 'Other', activityId: 2, id: 11, requireUserInput: true, sfdcRequired: true },
-  { name: 'Prepare for a customer demo', activityId: 3, id: 12, sfdcRequired: true },
-  { name: 'Prepare for a customer workshop', activityId: 3, id: 13, sfdcRequired: true },
-  { name: 'Learning about the product', activityId: 3, id: 14, sfdcRequired: false },
-  { name: 'Trying out a technical solution', activityId: 3, id: 15, sfdcRequired: false },
-  { name: 'Preparing for a customer services engagement', activityId: 3, id: 16, sfdcRequired: false },
-  { name: 'Conduct Internal Training/Enablement', activityId: 3, id: 17, sfdcRequired: false },
-  { name: 'Other', activityId: 3, id: 18, requireUserInput: true, sfdcRequired: true },
+  {
+    name: 'Show a demo to a customer',
+    description: 'Showing a product/solution to a customer or prospect',
+    activityId: 1,
+    id: 1,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Conduct a hands on workshop for one customer',
+    description: 'Performing a hands on activity with an individual customer or prospect',
+    activityId: 1,
+    id: 2,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Build a proof of concept for a customer',
+    description: 'Using an environment to build and show a customer or prospect the capabilities of Red Hat solutions',
+    activityId: 1,
+    id: 3,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Conduct a Marketing event with multiple customers',
+    description: 'Performing a demo or workshop for a Marketing event for multiple customers (tied to a campaign ID)',
+    activityId: 1,
+    id: 4,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Other',
+    description:
+      'Only select this option if the current purpose fields do not meet your needs. You will be prompted to add a description of your purpose in a text field so that we may better understand your reason for utilizing our platform',
+    activityId: 1,
+    id: 5,
+    requireUserInput: true,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Conduct a demo to a Partner',
+    description: 'Showing a product/solution to a Partner',
+    activityId: 2,
+    id: 6,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Conduct a demo with a Partner',
+    description: 'Showing a product/solution to a customer/prospect with a Partner',
+    activityId: 2,
+    id: 7,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Conduct an enablement workshop to Partners',
+    description: 'Performing a hands on activity with Partners',
+    activityId: 2,
+    id: 8,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Assist a Partner with a proof of concept',
+    description:
+      'Using an environment to build and show a customer or prospect the capabilities of Red Hat solutions with a Partner',
+    activityId: 2,
+    id: 9,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Assist a Partner with running an event for multiple customers',
+    description:
+      'Assist a Partner in performing a demo or workshop for multiple customers who share common business challenges',
+    activityId: 2,
+    id: 10,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Other',
+    description:
+      'Only select this option if the current purpose fields do not meet your needs. You will be prompted to add a description of your purpose in a text field so that we may better understand your reason for utilizing our platform',
+    activityId: 2,
+    id: 11,
+    requireUserInput: true,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Practice for a demo',
+    description: 'Setting up and practicing with an environment to be able to perform a customer demo',
+    activityId: 3,
+    id: 12,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Practice for a workshop',
+    description: 'Setting up and practicing with an environment to be able to conduct a customer workshop',
+    activityId: 3,
+    id: 13,
+    sfdcRequired: true,
+  },
+  {
+    name: 'Learning about the product',
+    description: 'Exploring Red Hat products in an active terminal to better understand their business value',
+    activityId: 3,
+    id: 14,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Trying out a technical solution',
+    description: 'Setting up an environment to test a potential product solution for a business challenge',
+    activityId: 3,
+    id: 15,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Preparing for a customer service engagement',
+    description: 'For consultants to set up and practice with an environment for a service solution',
+    activityId: 3,
+    id: 16,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Conduct internal training/enablement',
+    description: 'Using an environment to train team members/perform an internal Red Hat training',
+    activityId: 3,
+    id: 17,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Other',
+    description:
+      'Only select this option if the current purpose fields do not meet your needs. You will be prompted to add a description of your purpose in a text field so that we may better understand your reason for utilizing our platform',
+    activityId: 3,
+    id: 18,
+    requireUserInput: true,
+    sfdcRequired: true,
+  },
   { name: 'Tech Exchange', activityId: 4, id: 19, sfdcRequired: false },
   { name: 'Summit', activityId: 4, id: 20, sfdcRequired: false },
   { name: 'AnsibleFest', activityId: 4, id: 21, sfdcRequired: false },
   { name: 'Other', activityId: 4, id: 22, requireUserInput: true, sfdcRequired: false },
-  { name: 'Developing a Custom Demo', activityId: 5, id: 23, sfdcRequired: false },
-  { name: 'Developing a community demo', activityId: 5, id: 24, sfdcRequired: false },
-  { name: 'Other', activityId: 5, id: 25, requireUserInput: true, sfdcRequired: false },
-  { name: 'Asset Development', activityId: 6, id: 26, sfdcRequired: false },
-  { name: 'Asset Maintenance', activityId: 6, id: 27, sfdcRequired: false },
-  { name: 'QA', activityId: 6, id: 28, sfdcRequired: false },
-  { name: 'Other', activityId: 6, id: 29, requireUserInput: true, sfdcRequired: false },
+  {
+    name: 'Developing a custom demo',
+    description:
+      'For use when existing Red Hat Demo Platform assets do not meet your customer’s needs and you need to develop a custom demo to show a product’s business value; use when you anticipate the demo becoming an Enterprise Standard asset for the Demo Platform team to maintain and support (this means developing your code with AgnosticD/V)',
+    activityId: 5,
+    id: 23,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Developing a community demo',
+    description:
+      'For use when existing Red Hat Demo Platform assets do not meet your customer’s needs and you do not wish to develop your custom demo using AgnosticD/V; by developing a community demo, you agree to maintain the code in your own git repo. The Demo Platform team will maintain the base asset used to develop your community demo',
+    activityId: 5,
+    id: 24,
+    sfdcRequired: false,
+  },
+  {
+    name: 'Other',
+    description:
+      'Only select this option if the current purpose fields do not meet your needs. You will be prompted to add a description of your purpose in a text field so that we may better understand your reason for utilizing our platform',
+    activityId: 5,
+    id: 25,
+    requireUserInput: true,
+    sfdcRequired: false,
+  },
+  { name: 'Asset Development', description: '', activityId: 6, id: 26, sfdcRequired: false },
+  { name: 'Asset Maintenance', description: '', activityId: 6, id: 27, sfdcRequired: false },
+  { name: 'QA', description: '', activityId: 6, id: 28, sfdcRequired: false },
+  {
+    name: 'Other',
+    description:
+      'Only select this option if the current purpose fields do not meet your needs. You will be prompted to add a description of your purpose in a text field so that we may better understand your reason for utilizing our platform',
+    activityId: 6,
+    id: 29,
+    requireUserInput: true,
+    sfdcRequired: false,
+  },
 ];
 const ActivityPurposeSelector: React.FC<{
   onChange: (activity: string, purpose: string, explanation: string) => void;
@@ -114,7 +260,14 @@ const ActivityPurposeSelector: React.FC<{
                 <SelectOption
                   key={`${ActivityOpts.find((a) => a.id === purposeOpt.activityId).name} - ${purposeOpt.name}`}
                   value={purposeOpt.name}
-                />
+                >
+                  <span style={{ marginRight: 'var(--pf-global--spacer--sm)' }}>{purposeOpt.name}</span>
+                  {purposeOpt.description ? (
+                    <Tooltip position="right" content={<div>{purposeOpt.description}</div>}>
+                      <OutlinedQuestionCircleIcon aria-label={purposeOpt.description} className="tooltip-icon-only" />
+                    </Tooltip>
+                  ) : null}
+                </SelectOption>
               ))}
             </Select>
           </div>
