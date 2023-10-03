b58126c74864a8c3f83e47f4fa4277f9380f3664
Aleix <acasanov@redhat.com>
2023-10-03 11:51:37 +0200

    fix: Use integer values for new resourceClaim format (#1469)

--- catalog/ui/src/app/api.ts
+++ catalog/ui/src/app/api.ts
@@ -161,7 +161,7 @@ export async function assignWorkshopUser({
   workshop: Workshop;
 }) {
   const userAssignmentIdx: number = workshop.spec.userAssignments.findIndex(
-    (item) => resourceClaimName === item.resourceClaimName && userName === item.userName,
+    (item) => resourceClaimName === item.resourceClaimName && userName === item.userName
   );
   const userAssignment = workshop.spec.userAssignments[userAssignmentIdx];
   if (!userAssignment) {
@@ -283,7 +283,7 @@ export async function bulkAssignWorkshopUsers({
 
 export async function checkSalesforceId(
   id: string,
-  debouncedApiFetch: (path: string) => Promise<unknown>,
+  debouncedApiFetch: (path: string) => Promise<unknown>
 ): Promise<boolean> {
   if (!id) {
     return false;
@@ -413,7 +413,7 @@ export async function createServiceRequest({
 
     // Set annotation for parameter
     if (parameter.name && value !== undefined) {
-      requestResourceClaim.spec.provider.parameterValues[parameter.name] = String(value);
+      requestResourceClaim.spec.provider.parameterValues[parameter.name] = value;
     }
   }
 
@@ -589,7 +589,7 @@ export async function createWorkshopForMultiuserService({
   if (resourceClaim.status?.resourceHandle) {
     definition.metadata.labels[`${BABYLON_DOMAIN}/workshop-id`] = resourceClaim.status?.resourceHandle.name.replace(
       /^guid-/,
-      '',
+      ''
     );
   }
 
@@ -606,7 +606,7 @@ export async function createWorkshopForMultiuserService({
               [`${BABYLON_DOMAIN}/workshop`]: workshop.metadata.name,
             },
           },
-        },
+        }
       );
       return { resourceClaim: patchedResourceClaim, workshop: workshop };
     } catch (error: any) {
@@ -687,7 +687,7 @@ export async function getAnarchySubject(namespace: string, name: string) {
     'v1',
     namespace,
     'anarchysubjects',
-    name,
+    name
   )) as AnarchySubject;
 }
 
@@ -715,7 +715,7 @@ export async function getResourcePool(name: string) {
     'v1',
     'poolboy',
     'resourcepools',
-    name,
+    name
   )) as ResourcePool;
 }
 
@@ -764,7 +764,7 @@ export async function deleteAnarchyAction(anarchyAction: AnarchyAction) {
     'v1',
     anarchyAction.metadata.namespace,
     'anarchyactions',
-    anarchyAction.metadata.name,
+    anarchyAction.metadata.name
   );
 }
 
@@ -774,7 +774,7 @@ export async function deleteAnarchyGovernor(anarchyGovernor: AnarchyGovernor) {
     'v1',
     anarchyGovernor.metadata.namespace,
     'anarchygovernors',
-    anarchyGovernor.metadata.name,
+    anarchyGovernor.metadata.name
   );
 }
 
@@ -784,7 +784,7 @@ export async function deleteAnarchyRun(anarchyRun: AnarchyRun) {
     'v1',
     anarchyRun.metadata.namespace,
     'anarchyruns',
-    anarchyRun.metadata.name,
+    anarchyRun.metadata.name
   );
 }
 
@@ -794,7 +794,7 @@ export async function deleteAnarchySubject(anarchySubject: AnarchySubject) {
     'v1',
     anarchySubject.metadata.namespace,
     'anarchysubjects',
-    anarchySubject.metadata.name,
+    anarchySubject.metadata.name
   );
 }
 
@@ -821,7 +821,7 @@ export async function deleteResourceClaim(resourceClaim: ResourceClaim) {
     'v1',
     resourceClaim.metadata.namespace,
     'resourceclaims',
-    resourceClaim.metadata.name,
+    resourceClaim.metadata.name
   )) as ResourceClaim;
 }
 
@@ -831,7 +831,7 @@ export async function deleteResourceHandle(resourceHandle: ResourceHandle) {
     'v1',
     resourceHandle.metadata.namespace,
     'resourcehandles',
-    resourceHandle.metadata.name,
+    resourceHandle.metadata.name
   );
 }
 
@@ -841,7 +841,7 @@ export async function deleteResourcePool(resourcePool: ResourcePool) {
     'v1',
     resourcePool.metadata.namespace,
     'resourcepools',
-    resourcePool.metadata.name,
+    resourcePool.metadata.name
   );
 }
 
@@ -851,7 +851,7 @@ export async function deleteResourceProvider(resourceProvider: ResourceProvider)
     'v1',
     resourceProvider.metadata.namespace,
     'resourcehandles',
-    resourceProvider.metadata.name,
+    resourceProvider.metadata.name
   );
 }
 
@@ -886,7 +886,7 @@ export async function startWorkshop(workshop: Workshop, dateString: string, reso
         ? resourceClaim.status.resources
             .filter((r) => (r.state?.spec?.vars?.action_schedule?.default_runtime ? true : false))
             .map((r) => parseDuration(r.state.spec.vars.action_schedule.default_runtime))
-        : []),
+        : [])
     );
   }
   const patch = {
@@ -896,7 +896,7 @@ export async function startWorkshop(workshop: Workshop, dateString: string, reso
         stop: dateToApiString(
           defaultRuntimes.length > 0
             ? new Date(now.getTime() + Math.min(...defaultRuntimes))
-            : new Date(now.getTime() + 12 * 60 * 60 * 1000),
+            : new Date(now.getTime() + 12 * 60 * 60 * 1000)
         ),
       },
       lifespan: {
@@ -920,7 +920,7 @@ export async function startWorkshopServices(workshop: Workshop, resourceClaims: 
         ? resourceClaim.status.resources
             .filter((r) => (r.state?.spec?.vars?.action_schedule?.default_runtime ? true : false))
             .map((r) => parseDuration(r.state.spec.vars.action_schedule.default_runtime))
-        : []),
+        : [])
     );
   }
   const patch = {
@@ -930,7 +930,7 @@ export async function startWorkshopServices(workshop: Workshop, resourceClaims: 
         stop: dateToApiString(
           defaultRuntimes.length > 0
             ? new Date(now.getTime() + Math.min(...defaultRuntimes))
-            : new Date(now.getTime() + 12 * 60 * 60 * 1000),
+            : new Date(now.getTime() + 12 * 60 * 60 * 1000)
         ),
       },
     },
@@ -950,7 +950,7 @@ export async function forceDeleteAnarchySubject(anarchySubject: AnarchySubject) 
       anarchySubject.metadata.namespace,
       'anarchysubjects',
       anarchySubject.metadata.name,
-      { metadata: { finalizers: null } },
+      { metadata: { finalizers: null } }
     );
   }
   if (!anarchySubject.metadata.deletionTimestamp) {
@@ -1011,7 +1011,7 @@ export async function patchResourceClaim(namespace: string, name: string, patch:
     namespace,
     'resourceclaims',
     name,
-    patch,
+    patch
   )) as ResourceClaim;
 }
 
@@ -1022,7 +1022,7 @@ export async function patchResourcePool(name: string, patch: any) {
     'poolboy',
     'resourcepools',
     name,
-    patch,
+    patch
   )) as ResourcePool;
 }
 
@@ -1091,7 +1091,7 @@ export async function requestStatusForAllResourcesInResourceClaim(resourceClaim:
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    data,
+    data
   )) as ResourceClaim;
 }
 
@@ -1113,7 +1113,7 @@ export async function scheduleStopResourceClaim(resourceClaim: ResourceClaim, da
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    patch,
+    patch
   )) as ResourceClaim;
 }
 
@@ -1153,7 +1153,7 @@ export async function scheduleStopForAllResourcesInResourceClaim(resourceClaim: 
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    patch,
+    patch
   )) as ResourceClaim;
 }
 
@@ -1172,14 +1172,14 @@ export async function scheduleStartResourceClaim(resourceClaim: ResourceClaim, d
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    patch,
+    patch
   )) as ResourceClaim;
 }
 
 export async function scheduleStartForAllResourcesInResourceClaim(
   resourceClaim: ResourceClaim,
   date: Date,
-  stopDate: Date,
+  stopDate: Date
 ) {
   const startTimestamp = dateToApiString(date);
   const stopTimestamp = dateToApiString(stopDate);
@@ -1205,14 +1205,14 @@ export async function scheduleStartForAllResourcesInResourceClaim(
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    patch,
+    patch
   )) as ResourceClaim;
 }
 
 export async function setLifespanEndForResourceClaim(
   resourceClaim: ResourceClaim,
   date: Date,
-  updateResourceHandle = true,
+  updateResourceHandle = true
 ) {
   const endTimestamp = dateToApiString(date);
   const data = {
@@ -1226,7 +1226,7 @@ export async function setLifespanEndForResourceClaim(
     if (date.getTime() > maxDate.getTime()) {
       updatedMaxDate =
         Math.ceil(
-          (date.getTime() - new Date(resourceClaim.metadata.creationTimestamp).getTime()) / (1000 * 60 * 60 * 24),
+          (date.getTime() - new Date(resourceClaim.metadata.creationTimestamp).getTime()) / (1000 * 60 * 60 * 24)
         ) +
         1 +
         'd';
@@ -1267,7 +1267,7 @@ export async function setLifespanEndForResourceClaim(
               : {}),
           },
         },
-      },
+      }
     )) as ResourceHandle;
   }
 
@@ -1277,14 +1277,14 @@ export async function setLifespanEndForResourceClaim(
     resourceClaim.metadata.namespace,
     'resourceclaims',
     resourceClaim.metadata.name,
-    data,
+    data
   )) as ResourceClaim;
 }
 
 export async function startAllResourcesInResourceClaim(resourceClaim: ResourceClaim): Promise<ResourceClaim> {
   const defaultRuntimes = resourceClaim.status?.resources
     ? resourceClaim.status.resources.map((r) =>
-        parseDuration(r.state?.spec.vars.action_schedule?.default_runtime || '4h'),
+        parseDuration(r.state?.spec.vars.action_schedule?.default_runtime || '4h')
       )
     : [];
   const defaultRuntime = defaultRuntimes.length > 0 ? Math.min(...defaultRuntimes) : 0;
@@ -1303,7 +1303,7 @@ async function deleteNamespacedCustomObject(
   version: string,
   namespace: string,
   plural: string,
-  name: string,
+  name: string
 ): Promise<K8sObject> {
   const resp = await apiFetch(`/apis/${group}/${version}/namespaces/${namespace}/${plural}/${name}`, {
     method: 'DELETE',
@@ -1316,7 +1316,7 @@ async function getNamespacedCustomObject(
   version: string,
   namespace: string,
   plural: string,
-  name: string,
+  name: string
 ): Promise<K8sObject> {
   const resp = await apiFetch(`/apis/${group}/${version}/namespaces/${namespace}/${plural}/${name}`);
   return await resp.json();
@@ -1350,7 +1350,7 @@ async function patchNamespacedCustomObject(
   plural: string,
   name: string,
   patch: Record<string, unknown>,
-  patchType = 'merge',
+  patchType = 'merge'
 ): Promise<K8sObject> {
   const resp = await apiFetch(`/apis/${group}/${version}/namespaces/${namespace}/${plural}/${name}`, {
     method: 'PATCH',
@@ -1364,7 +1364,7 @@ async function patchNamespacedCustomObject(
 
 export async function getOpenStackServersForResourceClaim(resourceClaim: ResourceClaim) {
   const resp = await apiFetch(
-    `/api/service/${resourceClaim.metadata.namespace}/${resourceClaim.metadata.name}/openstack/servers`,
+    `/api/service/${resourceClaim.metadata.namespace}/${resourceClaim.metadata.name}/openstack/servers`
   );
   return await resp.json();
 }
@@ -1378,7 +1378,7 @@ export async function rebootOpenStackServer(resourceClaim: ResourceClaim, projec
       headers: {
         'Content-Type': 'application/json',
       },
-    },
+    }
   );
   return await resp.json();
 }
@@ -1392,7 +1392,7 @@ export async function startOpenStackServer(resourceClaim: ResourceClaim, project
       headers: {
         'Content-Type': 'application/json',
       },
-    },
+    }
   );
   return await resp.json();
 }
@@ -1406,7 +1406,7 @@ export async function stopOpenStackServer(resourceClaim: ResourceClaim, projectI
       headers: {
         'Content-Type': 'application/json',
       },
-    },
+    }
   );
   return await resp.json();
 }
@@ -1414,7 +1414,7 @@ export async function stopOpenStackServer(resourceClaim: ResourceClaim, projectI
 export async function startOpenStackServerConsoleSession(
   resourceClaim: ResourceClaim,
   projectId: string,
-  serverId: string,
+  serverId: string
 ) {
   const resp = await apiFetch(
     `/api/service/${resourceClaim.metadata.namespace}/${resourceClaim.metadata.name}/openstack/server/${projectId}/${serverId}/console`,
@@ -1424,7 +1424,7 @@ export async function startOpenStackServerConsoleSession(
       headers: {
         'Content-Type': 'application/json',
       },
-    },
+    }
   );
   return await resp.json();
 }
@@ -1491,7 +1491,7 @@ export function setProvisionRating(
   provisionUuid: string,
   rating: number,
   comment: string,
-  useful: 'yes' | 'no' | 'not applicable',
+  useful: 'yes' | 'no' | 'not applicable'
 ) {
   return apiFetch(apiPaths.PROVISION_RATING({ provisionUuid }), {
     method: 'POST',
