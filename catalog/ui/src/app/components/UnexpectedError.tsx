afda7859c8544409ee1eb7573e76180ac6aeaf98
Aleix Casanovas <acasanov@redhat.com>
2023-10-13 11:15:20 +0200

    fix: Use correct help link

--- catalog/ui/src/app/components/UnexpectedError.tsx
+++ catalog/ui/src/app/components/UnexpectedError.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { EmptyState, EmptyStateBody, EmptyStateIcon, Title } from '@patternfly/react-core';
 import ExclamationTriangleIcon from '@patternfly/react-icons/dist/js/icons/exclamation-triangle-icon';
+import { getHelpUrl } from '@app/util';
+import useSession from '@app/utils/useSession';
 
-const UnexpectedError: React.FC = () => (
-  <EmptyState variant="full">
-    <EmptyStateIcon icon={ExclamationTriangleIcon} />
-    <Title headingLevel="h1" size="lg">
-      Looks like something went wrong!
-    </Title>
-    <EmptyStateBody>
-      We track these errors automatically, but if the problem persists feel free to{' '}
-      <a href="https://red.ht/open-support" target="_blank" rel="noopener noreferrer">
-        contact us
-      </a>
-      .
-    </EmptyStateBody>
-  </EmptyState>
-);
-
+const UnexpectedError: React.FC = () => {
+  const { email } = useSession().getSession();
+  return (
+    <EmptyState variant="full">
+      <EmptyStateIcon icon={ExclamationTriangleIcon} />
+      <Title headingLevel="h1" size="lg">
+        Looks like something went wrong!
+      </Title>
+      <EmptyStateBody>
+        We track these errors automatically, but if the problem persists feel free to{' '}
+        <a href={getHelpUrl(email)} target="_blank" rel="noopener noreferrer">
+          contact us
+        </a>
+        .
+      </EmptyStateBody>
+    </EmptyState>
+  );
+};
 export default UnexpectedError;
