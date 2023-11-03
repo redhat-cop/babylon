615c348bd277534cdc1878d27aeeeb6e1f5caa74
Marcos Amorim <mamorim@redhat.com>
2023-11-03 13:04:50 -0400

    Enable JSON and access log, remove debug messages (#1550)

--- ratings/api/app.py
+++ ratings/api/app.py
@@ -1,15 +1,14 @@
-import os
 import logging
-from fastapi import FastAPI, Depends, HTTPException, Request
-from fastapi.responses import JSONResponse, HTMLResponse
-from babylon import Babylon
-from routers import ratings
+from datetime import datetime
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
 from models import Database as db
 from models.custom_base import create_tables
+from routers import ratings
+from babylon import Babylon
 
 
 logger = logging.getLogger('babylon-ratings')
-
 
 app = FastAPI()
 
@@ -25,6 +24,32 @@ async def on_startup():
 async def on_cleanup():
     await Babylon.on_cleanup()
     await db.shutdown()
+
+
+@app.middleware("http")
+async def log_access(request: Request, call_next):
+    log = logging.getLogger("uvicorn.access")
+
+    start_time = datetime.now()
+
+    response = await call_next(request)
+
+    end_time = datetime.now()
+    duration = (end_time - start_time).total_seconds() * 1000
+
+    log_data = {
+        "client_host": request.client.host,
+        "method": request.method,
+        "path": request.url.path,
+        "query": request.url.query,
+        "fragment": request.url.fragment,
+        "status_code": response.status_code,
+        "response_time": f"{duration}ms"
+    }
+
+    log.info(log_data)
+
+    return response
 
 
 @app.get("/", response_class=HTMLResponse)
