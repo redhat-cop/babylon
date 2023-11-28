05bd703817f6dd84d90feedd3d15caf999c16d7f
Aleix <acasanov@redhat.com>
2023-11-28 06:30:31 -0600

    fix: Fix broken UI tests (#1592)

--- catalog/ui/test-setup.ts
+++ catalog/ui/test-setup.ts
@@ -2,3 +2,7 @@ import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
+jest.mock(
+  'asciidoctor',
+  () => jest.fn(() => 'mocked') // <= ...this mock function
+);
