51653a393b80292219eeb53259c0ce65280ecbdc
Aleix Casanovas <acasanov@redhat.com>
2024-05-06 12:38:13 +0200

    feat: Status component

---
+++ catalog/status/main.js
@@ -0,0 +1,29 @@
+const interface = {
+    "demo.redhat.com": "/interfaces/rhpds.json",
+    "partner.demo.redhat.com": "/interfaces/rhdp-partners.json",
+    "default": "/interfaces/rhdp-partners.json"
+};
+
+(function() {
+    function getHtml(){
+        return document.documentElement.innerHTML;
+    }
+
+    function get(obj, desc) {
+        return obj[desc];
+    }
+
+    function replaceTokens(HTML, json) {
+        return HTML.split('{{').map(function(i) { 
+            var symbol = i.substring(0, i.indexOf('}}')).trim(); 
+            return i.replace(symbol + '}}', get(json, symbol)); 
+        }).join('');
+    }
+
+    fetch(interface[window.location.host] ?? interface.default)
+        .then((response) => response.json())
+        .then((json) => {
+            document.documentElement.innerHTML = replaceTokens(getHtml(), json)
+        });
+     
+})();
