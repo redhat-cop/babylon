aa9f7a8ccd5d3f8918abf06eed1535f55ca6ae7d
Aleix Casanovas <acasanov@redhat.com>
2024-06-07 14:02:58 +0200

    fixup! fixup! fixup! fixup! fixup! feat: autoDetach for workshopProvisions

--- catalog/ui/src/app/Catalog/CatalogItemForm.tsx
+++ catalog/ui/src/app/Catalog/CatalogItemForm.tsx
@@ -916,9 +916,9 @@ const CatalogItemFormData: React.FC<{ catalogItemName: string; catalogNamespaceN
                 Schedule{' '}
                 <span
                   style={{
-                    backgroundColor: submitRequestEnabled ? '#f0f0f0' : '#bee1f4',
+                    backgroundColor: !submitRequestEnabled ? '#f0f0f0' : '#bee1f4',
                     borderRadius: '10px',
-                    color: submitRequestEnabled ? '#8a8d90' : '#06c',
+                    color: !submitRequestEnabled ? '#8a8d90' : '#06c',
                     fontStyle: 'italic',
                     fontWeight: 300,
                     fontSize: '12px',
