b1d1b95eb19b417a03facfe4fefa9b9ebc9a4179
Aleix <acasanov@redhat.com>
2024-08-13 17:13:56 +0200

    Update main.js

--- catalog/status/main.js
+++ catalog/status/main.js
@@ -2,6 +2,8 @@ const interface = {
     "demo.redhat.com": "/status/interfaces/rhpds.json",
     "partner.demo.redhat.com": "/status/interfaces/rhdp-partners.json",
     "test.dev.demo.redhat.com": "/status/interfaces/rhpds.json",
+    "catalog.demo.redhat.com": "/status/interfaces/rhpds.json",
+    "catalog.partner.demo.redhat.com": "/status/interfaces/rhdp-partners.json",
     "default": "/status/interfaces/rhdp-partners.json"
 };
 
