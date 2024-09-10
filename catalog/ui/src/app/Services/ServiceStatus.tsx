764db12b53b07408672dd48aa6bce3f0084acfe2
Aleix <acasanov@redhat.com>
2024-09-10 10:43:00 +0200

    ref: Add missing state (#2114)

--- catalog/ui/src/app/Services/ServiceStatus.tsx
+++ catalog/ui/src/app/Services/ServiceStatus.tsx
@@ -92,6 +92,7 @@ const ServiceStatus: React.FC<{
       case summary.state === 'provisioning':
       case summary.state === 'requesting':
       case summary.state === 'initializing':
+      case summary.state === 'stopping':
         _phase = 'in-progress';
         break;
       case summary.state.endsWith('-failed'):
