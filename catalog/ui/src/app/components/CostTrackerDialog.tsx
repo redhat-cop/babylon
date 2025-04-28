a05398f74221d040b554a1da6a0309bb15b20d99
Aleix <acasanov@redhat.com>
2025-04-28 23:22:57 +0200

    fix: Update cost message with a tooltip (#2528)

--- catalog/ui/src/app/components/CostTrackerDialog.tsx
+++ catalog/ui/src/app/components/CostTrackerDialog.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from 'react';
 import { ResourceClaim } from '@app/types';
-import { EmptyState, EmptyStateIcon, EmptyStateHeader } from '@patternfly/react-core';
+import { EmptyState, EmptyStateIcon, EmptyStateHeader, Tooltip } from '@patternfly/react-core';
 import ErrorCircleOIcon from '@patternfly/react-icons/dist/js/icons/error-circle-o-icon';
 import { getCostTracker } from '@app/util';
 import useSWR from 'swr';
 import { apiPaths, fetchWithUpdatedCostTracker } from '@app/api';
 import LoadingIcon from '@app/components/LoadingIcon';
 import CurrencyAmount from './CurrencyAmount';
+import InfoAltIcon from '@patternfly/react-icons/dist/js/icons/info-alt-icon';
 
 const CostTrackerDialogData: React.FC<{
   resourceClaim: ResourceClaim;
@@ -37,7 +38,16 @@ const CostTrackerDialogData: React.FC<{
           fontStyle: 'italic',
         }}
       >
-        Estimated by the cloud provider
+        Estimated by the cloud provider (not live, discounts may apply)
+        <Tooltip content="This is an estimated cost based on data from the cloud provider. It may not reflect real-time usage or discounts. ">
+          <InfoAltIcon
+            style={{
+              paddingTop: 'var(--pf-v5-global--spacer--xs)',
+              marginLeft: 'var(--pf-v5-global--spacer--sm)',
+              width: 'var(--pf-v5-global--icon--FontSize--sm)',
+            }}
+          />
+        </Tooltip>
       </p>
     </div>
   ) : (
