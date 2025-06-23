0fe84b0920dead42ed54915282dad4bdef3c865b
Aleix Casanovas <acasanov@redhat.com>
2025-06-23 13:43:58 +0200

    feat: Allow alias to services for name differentiation

---
+++ catalog/ui/src/app/utils/useDebounceState.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+function useDebounceState(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounceState;
