510ef86f4410c5e5f4bcb09d8573ab3ba16116f1
Guillaume Coré <gucore@redhat.com>
2025-09-26 15:27:27 +0200

    Add missing default port for webhook

--- agnosticv-operator/operator/operator.py
+++ agnosticv-operator/operator/operator.py
@@ -44,7 +44,7 @@ async def on_startup(settings: kopf.OperatorSettings, logger, **_):
     configure_kopf_logging()
     
     # Start webhook server if enabled
-    webhook_port = int(os.environ.get('WEBHOOK_PORT', '8080'))
+    webhook_port = int(os.environ.get('WEBHOOK_PORT', '8090'))
     webhook_enabled = os.environ.get('WEBHOOK_ENABLED', 'true').lower() == 'true'
     
     if webhook_enabled:
