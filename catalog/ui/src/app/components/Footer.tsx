daf03a9d3ea91c8008f7a188fce664a2a8f3645e
Aleix <acasanov@redhat.com>
2026-02-05 19:15:18 +0100

    ref: Update footer typo (#3026)

--- catalog/ui/src/app/components/Footer.tsx
+++ catalog/ui/src/app/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: React.FC<{ rightElement?: ReactNode }> = ({ rightElement }) => (
         </a>
       </div>
       <div id="legal" className="footer__legal">
-        <div className="copyright">Copyright © 2026 Red Hat, Inc.</div>
+        <div className="copyright">Copyright © 2026 Red Hat</div>
         <div>
           <ul className="menu">
             <li className="first leaf">
