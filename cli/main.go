735a03e4ae4964f9e9481c5f33852520dff02158
Guillaume Coré <gucore@redhat.com>
2026-04-28 18:25:27 +0200

    feat(cli): add cross-compilation support and version command
    Enable macOS M1 (and other platform) users to build babylon-cli
    with `make babylon-cli-cross` in cli/. Follows the same pattern
    as sandbox-cli-cross.

--- cli/main.go
+++ cli/main.go
@@ -2,6 +2,12 @@ package main
 
 import "github.com/redhat-gpte/babylon/cli/cmd"
 
+// Build info, injected via ldflags
+var version = "development"
+var buildCommit = "HEAD"
+var buildTime = "undefined"
+
 func main() {
+	cmd.SetBuildInfo(version, buildCommit, buildTime)
 	cmd.Execute()
 }
