b26e3b308df19575c03b7184f23634167d84d793
Aleix <acasanov@redhat.com>
2026-05-05 21:51:44 +0200

    feat: Light services page (#3245)
    * feat: Light services page
    
    * fetch only single resourceClaims
    
    * async pages

--- catalog/ui/src/app/Services/renderWorkshopRow.tsx
+++ catalog/ui/src/app/Services/renderWorkshopRow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Spinner } from '@patternfly/react-core';
 import { ServiceActionActions, Workshop, WorkshopWithResourceClaims } from '@app/types';
 import TrashIcon from '@patternfly/react-icons/dist/js/icons/trash-icon';
 import { BABYLON_DOMAIN, displayName, getStageFromK8sObject } from '@app/util';
@@ -91,10 +92,13 @@ const renderWorkshopRow = ({
     </>
   );
   const guidCell = <span key="workshop-guid">-</span>;
+  const resourceClaimsLoaded = workshop.resourceClaims !== undefined;
   const activeResourceClaim = workshop.resourceClaims?.find((r) => !r.metadata.deletionTimestamp);
   const statusCell = (
     <>
-      {activeResourceClaim ? (
+      {!resourceClaimsLoaded ? (
+        <Spinner size="md" />
+      ) : activeResourceClaim ? (
         <ServiceStatus resourceClaim={activeResourceClaim} />
       ) : autoStartTime && autoStartTime > Date.now() ? (
         <span className="services-item__status--scheduled" key="scheduled">
