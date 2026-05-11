95ebddedeebc98945c2f8e533f12973957d7a009
Johnathan Kupferer <jkupfere@redhat.com>
2026-05-10 22:31:42 -0400

    working on fix-subjects.py

--- client/python/fix-subjects.py
+++ client/python/fix-subjects.py
@@ -3,6 +3,11 @@
 import asyncio
 
 from babylon_async import BabylonClient
+from babylon_async.exceptions import BabylonApiException
+
+from kubernetes_asyncio.client import (
+    ApiException as KubernetesApiException
+)
 
 async def fix_subjects(dry_run:bool=False, namespace:str|None=None):
     async with BabylonClient() as babylon:
@@ -16,6 +21,36 @@ async def fix_subject(anarchy_subject, dry_run=False):
 async def fix_runs(anarchy_subject, dry_run=False):
     oldest_queued_run = None
     has_active_run = False
+    #if anarchy_subject.status is not None:
+    #    status_patch = []
+    #    for idx, anarchy_run_ref in enumerate(anarchy_subject.status.runs.active):
+    #        anarchy_run = None
+    #        try:
+    #            anarchy_run = await anarchy_subject.get_anarchy_run(anarchy_run_ref.name)
+    #        except BabylonApiException as exception:
+    #            if exception.status != 404:
+    #                raise
+    #        if anarchy_run is None:
+    #            if dry_run:
+    #                print(f"Would remove missing AnarchyRun {anarchy_run_ref.name} from {anarchy_subject}")
+    #            else:
+    #                print(f"Removing missing AnarchyRun {anarchy_run_ref.name} from {anarchy_subject}")
+    #            status_patch.insert(0, {
+    #                "op": "remove",
+    #                "path": f"/status/runs/active/{idx}",
+    #            })
+    #            status_patch.insert(0, {
+    #                "op": "test",
+    #                "path": f"/status/runs/active/{idx}/name",
+    #                "value": anarchy_run_ref.name,
+    #            })
+    #    if len(status_patch) > 0 and not dry_run:
+    #        print(status_patch)
+    #        try:
+    #            await anarchy_subject.patch_status(status_patch)
+    #        except KubernetesApiException as exception:
+    #            if exception.status not in (404, 422):
+    #                raise
     async for anarchy_run in anarchy_subject.list_anarchy_runs():
         if anarchy_run.is_active:
             has_active_run = True
