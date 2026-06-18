29ba57df0d7a5c9d1daf538e5913c066fed28da7
Aleix <acasanov@redhat.com>
2026-06-18 10:54:13 +0200

    Feat/catalog manager use reporting api (#3433)

--- catalog-manager/operator/catalog_item_service.py
+++ catalog-manager/operator/catalog_item_service.py
@@ -4,16 +4,7 @@ import aiohttp
 from datetime import datetime
 from rating import Rating
 from babylon import Babylon
-from utils import execute_query
 
-GET_CATALOG_ITEM_LAST_SUCCESSFUL_PROVISION = """SELECT to_char(provisions.provisioned_at, 'YYYY-MM-DD"T"HH24:MI:SS"') AS last_successful_provision
-        FROM catalog_items
-            JOIN provisions 
-            ON catalog_items.id = provisions.catalog_id
-        WHERE catalog_items.asset_uuid=(%s)
-        AND provision_result = 'success'
-        ORDER BY provisions.provisioned_at DESC
-        LIMIT 1;"""
 
 class ProvisionData:
     def __init__(self, last_successful_provision):
@@ -27,18 +18,31 @@ class CatalogItemService:
 
     async def get_provision_data(self):
         last_successful_provision = None
-        if len(self.catalog_item.labels['gpte.redhat.com/asset-uuid']) != 0:
-            query = await execute_query(
-                GET_CATALOG_ITEM_LAST_SUCCESSFUL_PROVISION, (self.catalog_item.labels['gpte.redhat.com/asset-uuid'], )
-            )
-            resultArr = query.get("result", [])
-            if len(resultArr) > 0:
-                _last_successful_provision_str = resultArr[0].get(
-                    "last_successful_provision", None
-                )
-                if (_last_successful_provision_str is not None):
-                    self.logger.info(_last_successful_provision_str)
-                    last_successful_provision = datetime.fromisoformat(_last_successful_provision_str)
+        asset_uuid = self.catalog_item.labels.get('gpte.redhat.com/asset-uuid', '')
+        stage = self.catalog_item.labels.get('babylon.gpte.redhat.com/stage', '')
+        if asset_uuid:
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        f"{Babylon.reporting_api}/catalog_item/metrics/{asset_uuid}?environment={stage}",
+                        headers={"Authorization": f"Bearer {Babylon.reporting_api_authorization_token}"}
+                    ) as resp:
+                        if resp.status == 200:
+                            self.logger.info(
+                                f"/catalog_item/metrics/{asset_uuid}?environment={stage} - {resp.status}"
+                            )
+                            response = await resp.json()
+                            _last_successful_provision_str = response.get("lastSuccessfulProvision")
+                            if _last_successful_provision_str is not None:
+                                self.logger.info(_last_successful_provision_str)
+                                last_successful_provision = datetime.fromisoformat(_last_successful_provision_str)
+                        else:
+                            self.logger.warning(
+                                f"/catalog_item/metrics/{asset_uuid}?environment={stage} - {resp.status}"
+                            )
+            except Exception as e:
+                self.logger.error(f"Invalid connection with {Babylon.reporting_api} - {e}")
+                raise
         return ProvisionData(last_successful_provision)
 
     async def get_rating_from_api(self):
